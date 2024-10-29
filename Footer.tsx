1edc9efa80244866f76447c341ab14ba93d2a12c
Iffat mumtaz <160539573+iffatmumtaz@users.noreply.github.com>
2024-10-30 01:57:59 +0500

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,14 @@
+
+     
+const Footer = () => {
+    return (
+        <div className="items-center flex-col gap-5 px-5 py-3 shadow-3xl bg-gray-400">
+            <div className="font-serif">
+                <p>Reserved © 2024</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
+
---
+++ Header.tsx
@@ -0,0 +1,18 @@
+
+const Header =()=>{
+    return(
+        <div className="flex justify-between px-5 shadow-3xl bg-gray-400">
+        <div>LOGO</div>
+
+    <div className="flex gap-4">
+        <div className="cursorpointer">Home</div>  
+        <div className="cursorpointer">About</div>  
+        <div className="cursorpointer">Services</div>  
+        <div className="cursorpointer">Contact us</div>  
+        </div>
+      
+    </div>
+    )
+};
+export default Header;
+/*simple figma templtes for developers*/
